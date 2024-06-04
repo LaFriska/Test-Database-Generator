ad52faebabd49d374a0dad539f32bc901c449d4e
Harold Gao <harold.gao205@gmail.com>
2024-06-04 13:06:24 +1000

    Writing initial statements.

---
+++ create_db.py
@@ -0,0 +1,44 @@
+import json
+
+courses = json.load(open("courses.json"))
+
+initial = """
+--WARNING!
+--DO NOT EXECUTE THIS FILE ON A DATABASE SERVER IF
+--TABLES WITH THE NAME "course", "staff" and "teach"
+--ARE KEPT, UNLESS THESE TABLES ARE FOR THE SOLE PURPOSE
+--OF COMP2400 EXAM REVISION. RUNNING THIS FILE DROPS ALL
+--THREE TABLES.
+
+DROP TABLE course CASCADE;
+DROP TABLE staff CASCADE;
+DROP TABLE teach CASCADE;
+
+create table staff (
+  uid char(8) primary key,
+  name text not null
+);
+create table course (
+  ccode char(8) primary key,
+  name text not null,
+  level integer
+);
+create table teach (
+  uid char(8) not null references staff(uid),
+  ccode char(8) not null references course(ccode),
+  sem char(2) check (sem = 'S1' or sem = 'S2'), -- sem can only take values 'S1' or 'S2'
+  year integer,
+  role text not null,
+  primary key (uid, ccode, sem, year)
+);
+"""
+
+sqlFile = open('testdb.sql', 'w')
+
+#Writes initial statements.
+sqlFile.write(initial)
+
+
+
+sqlFile.close()
+
---
+++ test.sql
@@ -0,0 +1,1 @@
+INSERT INTO course (ccode, name, level) VALUES ('COMP2400', 'Relational Databases', 2000);
